99b0891606bf0db868b21c697bce078e29ac6d37
Altrue <altrue@hotmail.fr>
2015-11-13 10:37:16 +0100

    Added lock dragmove button

--- Overview/MainWindow.xaml.cs
+++ Overview/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Overview
 {
@@ -26,40 +27,91 @@ namespace Overview
 
         // Constants
         private const int WINDOW_WIDTH = 150;           // in pixels
-        private const int WINDOW_HEIGHT = 400;           // in pixels
+        private const int WINDOW_HEIGHT = 400;          // in pixels
 
+        // Variables
+        private bool isDragMovable = true;
+
+        // Handle Recuperation Tools
         [DllImport("user32.dll")]
         static extern IntPtr GetDesktopWindow();
 
         [DllImport("user32.dll")]
         public static extern IntPtr SetParent(
-            IntPtr hWndChild,      // handle to window
-            IntPtr hWndNewParent   // new parent window
+            IntPtr hWndChild,      // Child Handle
+            IntPtr hWndNewParent   // New Parent Handle
         );
 
         public MainWindow()
         {
-            IntPtr hwndf = Process.GetCurrentProcess().MainWindowHandle;
+            // Handle recuperation for current window
+            GCHandle handle1 = GCHandle.Alloc(Process.GetCurrentProcess().MainWindowHandle);
+            IntPtr hwndf = (IntPtr) handle1;
             IntPtr hwndParent = GetDesktopWindow();
             SetParent(hwndf, hwndParent);
 
+            // Initialization
             InitializeComponent();
             Width = WINDOW_WIDTH;
             Height = WINDOW_HEIGHT;
-            ResizeMode = ResizeMode.NoResize;
+            ResizeMode = ResizeMode.CanMinimize;
             WindowStyle = WindowStyle.None;
             Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x22, 0x22, 0x22));
+
+            // Event assignation
+            MouseDown += MainWindow_MouseDown;
+            Loaded += MainWindowLoaded;
+
+            // Main Canvas Initialization
+            MainCanvas.Width = WINDOW_WIDTH;
+            MainCanvas.Height = WINDOW_HEIGHT;
+            MainCanvas.HorizontalAlignment = HorizontalAlignment.Left;
+            MainCanvas.VerticalAlignment = VerticalAlignment.Top;
+
+            // Lock button
+            Rectangle bt_Lock = new Rectangle();
+            bt_Lock.Width = 25;
+            bt_Lock.Height = 25;
+            bt_Lock.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x44, 0x22, 0x22));
+            bt_Lock.MouseDown += LockButton_MouseDown;
+            Canvas.SetTop(bt_Lock, (0));
+            Canvas.SetRight(bt_Lock, (0));
+            MainCanvas.Children.Add(bt_Lock);
         }
 
+        // Timer
+        void MainWindowLoaded(object _sender, RoutedEventArgs _e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 200); // Timer Length
+            dispatcherTimer.Start();
+        }
+
+        // Action to execute each tick
+        void dispatcherTimer_Tick(object sender, EventArgs _e)
+        {
+            if (WindowState != WindowState.Normal)
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        // Lock / Unlock
+        public void LockButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragMovable = (isDragMovable == true ? false : true);
+            Rectangle bt_Lock = (Rectangle)e.Source;
+            bt_Lock.Fill = new SolidColorBrush((isDragMovable == true ? Color.FromArgb(0xFF, 0x88, 0x22, 0x22) : Color.FromArgb(0xFF,0x44, 0x22, 0x22)));
+        }
+
+        // DragMove
         public void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
-        }
-
-        private void Window_Deactivated(object sender, EventArgs e)
-        {
-            Window window = (Window)sender;
-            window.Topmost = true;
+            if (isDragMovable == true)
+            {
+                DragMove();
+            }
         }
     }
 }
