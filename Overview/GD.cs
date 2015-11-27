dd3f799fa01b83d2126db52a6b01f95b2d76189c
Altrue <altrue@hotmail.fr>
2015-11-27 12:53:30 +0100

    Cleanup followup

--- Overview/GD.cs
+++ Overview/GD.cs
@@ -26,32 +26,30 @@ namespace Overview
     // ===============================
     public static class GD
     {
-        // ===============================
-        // DEBUG MODE CONTROL
+        // ========== DEBUG MODE CONTROL ==========
         public const bool DEBUGMODE = false;
-        // ===============================
+        // ========================================
 
-        // Constants
-        public const int WINDOW_WIDTH = 150;
-        public const int WINDOW_HEIGHT = 400;
-
-        // Variables
-        public static int coreNumber = 0;
+        // Constants & Variables
+            // - Constants
+            public const int WINDOW_WIDTH = 150;
+            public const int WINDOW_HEIGHT = 400;
+            // - Variables
+            public static int coreNumber = 0;
 
         // UI Elements
-        public static Canvas GraphCanvasCPU = new Canvas();
-        public static Brush[] brushes = { Brushes.IndianRed, Brushes.OrangeRed, Brushes.Gold, Brushes.LightGreen, Brushes.LightCyan, Brushes.LightBlue, Brushes.LightSteelBlue, Brushes.GhostWhite, Brushes.LightPink, Brushes.Pink, Brushes.Purple, Brushes.DarkRed, Brushes.DarkOrange, Brushes.DarkGoldenrod, Brushes.DarkGreen, Brushes.DarkSlateBlue, Brushes.DarkSeaGreen };
+            // - Canvas
+            public static Canvas GraphCanvasCPU = new Canvas();
+            public static Canvas MainCanvas = new Canvas();
+            // - Brushes
+            public static Brush[] brushes = { Brushes.IndianRed, Brushes.OrangeRed, Brushes.Gold, Brushes.LightGreen, Brushes.LightCyan, Brushes.LightBlue, Brushes.LightSteelBlue, Brushes.GhostWhite, Brushes.LightPink, Brushes.Pink, Brushes.Purple, Brushes.DarkRed, Brushes.DarkOrange, Brushes.DarkGoldenrod, Brushes.DarkGreen, Brushes.DarkSlateBlue, Brushes.DarkSeaGreen };
 
-        // Dictionaries
-        public static Dictionary<Int16, List<int>> CoreData = new Dictionary<Int16, List<int>>();
-        public static Dictionary<Int16, Polyline> CPUPolyLines = new Dictionary<Int16, Polyline>();
-
-        // Arrays
-        public static PerformanceCounter[] pcArrayCPU = new PerformanceCounter[17];
-        public static TextBlock[] tbArrayCPU = new TextBlock[32];
-
-        // Canvas
-        public static Canvas MainCanvas = new Canvas();
-
+        // Data Storage
+            // - Dictionaries
+            public static Dictionary<Int16, List<int>> CoreData = new Dictionary<Int16, List<int>>();
+            public static Dictionary<Int16, Polyline> CPUPolyLines = new Dictionary<Int16, Polyline>();
+            // - Arrays
+            public static PerformanceCounter[] pcArrayCPU = new PerformanceCounter[17];
+            public static TextBlock[] tbArrayCPU = new TextBlock[32];
     }
 }
